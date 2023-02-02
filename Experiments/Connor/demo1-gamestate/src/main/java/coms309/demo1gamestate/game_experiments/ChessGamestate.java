0129d914d6dc0a79969a2245aa396d20b5fa52df
Connor <connor66@iastate.edu>
2023-02-01 19:52:43 -0600

    Making changes to my experiment

--- Experiments/Connor/demo1-gamestate/src/main/java/coms309/demo1gamestate/game_experiments/ChessGamestate.java
+++ Experiments/Connor/demo1-gamestate/src/main/java/coms309/demo1gamestate/game_experiments/ChessGamestate.java
@@ -1,4 +1,4 @@
 package coms309.demo1gamestate.game_experiments;
 
-public class UserProfile {
+public class ChessGamestate {
 }
---
+++ Experiments/Connor/demo1-gamestate/src/main/java/coms309/demo1gamestate/game_experiments/ChessGamestateController.java
@@ -0,0 +1,2 @@
+package coms309.demo1gamestate.game_experiments;public class ChessGameController {
+}
