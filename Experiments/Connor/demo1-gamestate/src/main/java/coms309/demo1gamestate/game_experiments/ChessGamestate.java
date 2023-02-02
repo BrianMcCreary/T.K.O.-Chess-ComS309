28dcd10d76d8ad92c244708c24d052b799ebd808
Connor <connor66@iastate.edu>
2023-02-01 22:30:11 -0600

    Working on experiments

--- Experiments/Connor/demo1-gamestate/src/main/java/coms309/demo1gamestate/game_experiments/ChessGamestate.java
+++ Experiments/Connor/demo1-gamestate/src/main/java/coms309/demo1gamestate/game_experiments/ChessGamestate.java
@@ -1,4 +1,44 @@
 package coms309.demo1gamestate.game_experiments;
 
 public class ChessGamestate {
+    private String Gamestate;
+    private String playerB;
+    private String playerW;
+
+    public ChessGamestate() {}
+
+    public ChessGamestate(String Gamestate, String playerB, String playerW) {
+        this.Gamestate = Gamestate;
+        this.playerB = playerB;
+        this.playerW = playerW;
+    }
+
+    public String getPlayerB() {
+        return playerB;
+    }
+
+    public void setPlayerB(String playerB) {
+        this.playerB = playerB;
+    }
+
+    public String getPlayerW() {
+        return playerW;
+    }
+
+    public void setPlayerW(String playerW) {
+        this.playerW = playerW;
+    }
+
+    public String getGamestate() {
+        return Gamestate;
+    }
+
+    public void setGamestate(String Gamestate) {
+        this.Gamestate = Gamestate;
+    }
+
+    @Override
+    public String toString() {
+        return "PlayerW: " + playerW + "\nPlayerB: " + playerB + "\nGamestate: " + Gamestate;
+    }
 }
--- Experiments/Connor/demo1-gamestate/src/main/java/coms309/demo1gamestate/game_experiments/ChessGamestateController.java
+++ Experiments/Connor/demo1-gamestate/src/main/java/coms309/demo1gamestate/game_experiments/ChessGamestateController.java
@@ -1,2 +1,18 @@
-package coms309.demo1gamestate.game_experiments;public class ChessGameController {
+package coms309.demo1gamestate.game_experiments;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class ChessGamestateController {
+    ChessGamestate game = new ChessGamestate();
+
+    @PostMapping("/ChessGamestate")
+    public @ResponseBody String createChessGamestate(@RequestBody ChessGamestate gamestate) {
+        System.out.println(gamestate.toString());
+        game.setGamestate(gamestate.getGamestate());
+        game.setPlayerW(gamestate.getPlayerW());
+        game.setPlayerB(gamestate.getPlayerB());
+        return "Following Gamestate has been saved:\n" + gamestate.toString();
+    }
 }
