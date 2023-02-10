3589fc649705ebbb7c65258edeb9133bbe7c397d
Connor Hand <connor66@iastate.edu>
2023-02-10 15:42:33 -0600

    Removing duplicated files

--- Experiments/Connor/demo1Chess/src/main/java/coms309/demo1Chess/WelcomeController.java
+++
@@ -1,9 +0,0 @@
-package coms309.demo1Chess;
-import org.springframework.web.bind.annotation.*;
-@RestController
-public class WelcomeController {
-    @GetMapping("/")
-    public @ResponseBody String welcome() {
-        return "Welcome to chess.";
-    }
-}
