ee844483d75478ecf4a6b9c367e3611e390f64c3
Connor Hand <connor66@iastate.edu>
2023-02-20 15:19:56 -0600

    Adding project to backend folder

---
+++ Backend/Total-Knockout-Chess/src/main/java/coms309/an5/Total/Knockout/Chess/TotalKnockoutChessApplication.java
@@ -0,0 +1,13 @@
+package coms309.an5.Total.Knockout.Chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotalKnockoutChessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotalKnockoutChessApplication.class, args);
+	}
+
+}
