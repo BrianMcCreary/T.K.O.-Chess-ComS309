5046bb60850262a48d1ad6f79c1cf0c761cd0dad
Connor Hand <connor66@iastate.edu>
2023-02-24 16:02:05 -0600

    Creating new project for backend

---
+++ Backend/Total-Knockout-Chess/src/test/java/coms309/total_knockout_chess/TotalKnockoutChessApplicationTests.java
@@ -0,0 +1,13 @@
+package coms309.total_knockout_chess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotalKnockoutChessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
