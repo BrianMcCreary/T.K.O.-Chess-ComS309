7311856567ade9bc1ea60710ca5bda0ddf490701
Connor <connor66@iastate.edu>
2023-02-25 15:27:54 -0600

    Chagnes to UserController and pom.xml

--- Backend/Total-Knockout-Chess/src/main/java/Users/UserController.java
+++ Backend/Total-Knockout-Chess/src/main/java/Users/UserController.java
@@ -1,8 +1,34 @@
 package Users;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
+    @Autowired
+    UserRepository userRepository;
 
+    private String success = "{\"message\":\"success\"}";
+    private String failure = "{\"message\":\"failure\"}";
+
+    @GetMapping(path = "/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping(path = "/users")
+    String createUser(@RequestBody User user) {
+        if (user == null) {
+            return failure;
+        }
+        userRepository.save(user);
+        return success;
+    }
+
+    @GetMapping(path = "/users/{id}")
+    User getUserById(@PathVariable int id) {
+        return userRepository.findById(id);
+    }
 }
--- Backend/Total-Knockout-Chess/src/main/java/Users/UserRepository.java
+++ Backend/Total-Knockout-Chess/src/main/java/Users/UserRepository.java
@@ -5,4 +5,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findById(int id);
+
+    void deleteById(int id);
+
+    void save(User user);
 }
