093713f5308b43a557f2470a0ac8f43fd681db6b
Zachary Scurlock <scurlock@iastate.edu>
2023-02-25 16:00:44 -0600

    added main menu screen with buttons

---
+++ Frontend/T.K.O_Chess/app/src/main/java/com/example/tko_chess/MainMenuActivity.java
@@ -0,0 +1,26 @@
+package com.example.tko_chess;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainMenuActivity {
+
+    Button tkoChess;
+    Button chess;
+    Button Boxing;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+    }
+
+    private void setContentView(int activity_menu) {
+    }
+}
