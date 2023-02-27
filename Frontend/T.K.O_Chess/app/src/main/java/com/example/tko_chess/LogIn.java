52d23bd97410acaafb7869fc8800c4d983e2354c
Lex <aesomers@iastate.edu>
2023-02-27 12:30:34 -0600

    Started implementing round trip java code

--- Frontend/T.K.O_Chess/app/src/main/java/com/example/tko_chess/LogIn.java
+++ Frontend/T.K.O_Chess/app/src/main/java/com/example/tko_chess/LogIn.java
@@ -2,6 +2,9 @@ package com.example.tko_chess;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LogIn extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class LogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Login button that takes users to main menu after inputting a username and password
+        Button Login = (Button) findViewById(R.id.LoginButton);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Text fields for users to enter username/password for their account
+                EditText Username = (EditText) findViewById(R.id.UsernameText);
+                EditText Password = (EditText) findViewById(R.id.PasswordText);
+
+                //Strings containing username/password. Used to check that user does exist in database.
+                String username = Username.getText().toString();
+                String password = Username.getText().toString();
+
+
+            }
+        });
+
     }
 }
