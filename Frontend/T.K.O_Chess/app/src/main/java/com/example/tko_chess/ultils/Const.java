8d7d3a7d31bb581a05dc54b0fe2e5874e93123a7
Zachary Scurlock <scurlock@iastate.edu>
2023-03-02 17:46:14 -0600

    prepping for merge from main

--- Frontend/T.K.O_Chess/app/src/main/java/com/example/tko_chess/ultils/Const.java
+++ Frontend/T.K.O_Chess/app/src/main/java/com/example/tko_chess/ultils/Const.java
@@ -3,6 +3,12 @@ package com.example.tko_chess.ultils;
 public class Const {
 
     public static final String URL_SERVER_AN5 = "coms-309-005.class.las.iastate.edu";
+    public static final String URL_SERVER_USERS = "http://coms-309-005.class.las.iastate.edu:8080/users";
+
+    public static final String URL_SERVER_LOGIN = "http://coms-309-005.class.las.iastate.edu:8080/users/login";
+
+    public static final String URL_SERVER_CHANGEUSERNAME = "http://coms-309-005.class.las.iastate.edu:8080/users/name/";
+
 }
 
 
