5675f65df2dbf243a11ca3656cc69bfa4bca09b0
scurlock <scurlock@iastate.edu>
2023-03-30 18:03:16 -0500

    adding files to laptop

--- Frontend/T.K.O_Chess/app/src/main/java/com/example/tko_chess/chessboard.java
+++ Frontend/T.K.O_Chess/app/src/main/java/com/example/tko_chess/chessboard.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
