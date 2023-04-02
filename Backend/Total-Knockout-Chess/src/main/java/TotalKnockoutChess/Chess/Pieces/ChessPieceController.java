04799dd2e89afb830f7a78ac20f257c1a63f04b3
Brian McCreary <brianmcc@iastate.edu>
2023-04-01 21:09:28 -0500

    April 1st chess logic progress

---
+++ Backend/Total-Knockout-Chess/src/main/java/TotalKnockoutChess/Chess/Pieces/ChessPieceController.java
@@ -0,0 +1,21 @@
+package TotalKnockoutChess.Chess.Pieces;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChessPieceController {
+    /**
+     * timer
+     * calculateAvailableMoves
+     * makeMove
+     */
+
+
+    @GetMapping()
+    public List<> calculateAvailableMoves(){
+        
+    }
+
+
+}
