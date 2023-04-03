a0f54b4191865c3b716d0e2abccda46ed71285d7
Connor Hand <connor66@iastate.edu>
2023-04-03 15:14:40 -0500

    Adding friend implementation files to this branch

---
+++ Backend/Total-Knockout-Chess/src/main/java/TotalKnockoutChess/Friends/FriendshipRepository.java
@@ -0,0 +1,12 @@
+package TotalKnockoutChess.Friends;
+
+import TotalKnockoutChess.Users.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
+    Friendship findById(int id);
+
+    void deleteById(int id);
+}
