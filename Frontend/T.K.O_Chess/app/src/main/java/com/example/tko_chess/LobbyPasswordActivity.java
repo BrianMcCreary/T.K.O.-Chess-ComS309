d5670d34694a3a0ec347f319701ab17bfef4684f
Zachary Scurlock <scurlock@iastate.edu>
2023-04-04 15:26:16 -0500

    getting ready to merge lex's branch to main

--- Frontend/T.K.O_Chess/app/src/main/java/com/example/tko_chess/LobbyPasswordActivity.java
+++ Frontend/T.K.O_Chess/app/src/main/java/com/example/tko_chess/LobbyPasswordActivity.java
@@ -1,4 +1,47 @@
 package com.example.tko_chess;
 
-public class LobbyPasswordActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LobbyPasswordActivity extends AppCompatActivity {
+
+    EditText lobbyKey;
+    Button joinBtn;
+    ImageButton backBtn;
+    TextView error;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lobby_password);
+
+        backBtn = findViewById(R.id.backBtn8);
+
+        //Goes back to Host or Join Screen
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LobbyPasswordActivity.this, HostOrJoinActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        joinBtn = findViewById(R.id.joinBtn2);
+
+        //Takes user to joined lobby
+        joinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
 }
