29a60a3410060e1adcf8fd01facd41f709f0f651
Zachary Scurlock <scurlock@iastate.edu>
2023-04-08 17:58:40 -0500

    fixing random problem

--- Backend/Total-Knockout-Chess/src/main/java/TotalKnockoutChess/Boxing/BoxingGameController.java
+++ Backend/Total-Knockout-Chess/src/main/java/TotalKnockoutChess/Boxing/BoxingGameController.java
@@ -3,8 +3,9 @@ package TotalKnockoutChess.Boxing;
 import TotalKnockoutChess.Users.User;
 import TotalKnockoutChess.Users.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class BoxingGameController {
@@ -20,28 +21,39 @@ public class BoxingGameController {
 
     //Method for creating the boxing game
     @PostMapping(path = "/boxingGame/{player1}/{player2}")
-    public String createBoxingGame(String player1, String player2) {
-        User p1 = null;
-        User p2 = null;
+    public String createBoxingGame(@PathVariable String player1, @PathVariable String player2) {
         int tester = 0;
 
         //Loop through all users and find the two players
         for (User user : userRepository.findAll()) {
             if (user.getUsername().equals(player1)) {
-                p1 = user;
                 tester++;
             }
             else if (user.getUsername().equals(player2)) {
-                p2 = user;
                 tester++;
             }
         }
         if (tester != 2) {
             return failure;     //Users not found
         }
-        BoxingGame game = new BoxingGame(p1, p2);
+        BoxingGame game = new BoxingGame(player1, player2);
         boxingGameRepository.save(game);
         return success;
     }
 
+    @PutMapping(path = "/boxingGame/{player1}/{player2}")
+    public String deleteBoxingGame(@PathVariable String player1, @PathVariable String player2) {
+        for (BoxingGame bg : boxingGameRepository.findAll()) {
+            if ((bg.getPlayer1().equals(player1) && bg.getPlayer2().equals(player2)) || (bg.getPlayer1().equals(player2) && bg.getPlayer2().equals(player1))) {
+                boxingGameRepository.delete(bg);
+                return success;
+            }
+        }
+        return failure;
+    }
+
+    @GetMapping(path = "/getBoxingGames")
+    public List<BoxingGame> getBoxingGames() {
+        return boxingGameRepository.findAll();
+    }
 }
