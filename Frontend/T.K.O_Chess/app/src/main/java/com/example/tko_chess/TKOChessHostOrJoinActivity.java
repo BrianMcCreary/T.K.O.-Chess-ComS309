79630031e5e64a67fbbf0e3bb405b982369bd904
Lex <aesomers@iastate.edu>
2023-04-14 17:50:57 -0500

    done on laptop, moving to desktop

--- Frontend/T.K.O_Chess/app/src/main/java/com/example/tko_chess/TKOChessHostOrJoinActivity.java
+++ Frontend/T.K.O_Chess/app/src/main/java/com/example/tko_chess/TKOChessHostOrJoinActivity.java
@@ -23,39 +23,4 @@ public class TKOChessHostOrJoinActivity extends AppCompatActivity {
     ImageButton backBtn;
     String URLConcatenation = "";
     public String lobbyKey = "";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tko_chess_host_or_join);
-        backBtn = findViewById(R.id.backBtn8);
-
-        backBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(TKOChessHostOrJoinActivity.this, MainMenuActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        hostBtn = findViewById(R.id.hostBtn2);
-        hostBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(TKOChessHostOrJoinActivity.this, TKOChessLobbyActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        joinBtn = findViewById(R.id.joinBtn);
-        joinBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(TKOChessHostOrJoinActivity.this, TKOChessLobbyKeyActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-    }
 }
