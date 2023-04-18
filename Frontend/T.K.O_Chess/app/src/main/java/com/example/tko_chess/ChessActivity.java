931a53f3e5c4ae48cd9ca37a53cd871bfe4e2f74
Zachary Scurlock <scurlock@iastate.edu>
2023-04-17 20:06:16 -0500

    added enable button and disable button method

--- Frontend/T.K.O_Chess/app/src/main/java/com/example/tko_chess/ChessActivity.java
+++ Frontend/T.K.O_Chess/app/src/main/java/com/example/tko_chess/ChessActivity.java
@@ -23,6 +23,70 @@ import com.example.tko_chess.ultils.Const;
 
 public class ChessActivity extends AppCompatActivity {
 
+    ImageButton A1;
+    ImageButton B1;
+    ImageButton C1;
+    ImageButton D1;
+    ImageButton E1;
+    ImageButton F1;
+    ImageButton G1;
+    ImageButton H1;
+    ImageButton A2;
+    ImageButton B2;
+    ImageButton C2;
+    ImageButton D2;
+    ImageButton E2;
+    ImageButton F2;
+    ImageButton G2;
+    ImageButton H2;
+    ImageButton A3;
+    ImageButton B3;
+    ImageButton C3;
+    ImageButton D3;
+    ImageButton E3;
+    ImageButton F3;
+    ImageButton G3;
+    ImageButton H3;
+    ImageButton A4;
+    ImageButton B4;
+    ImageButton C4;
+    ImageButton D4;
+    ImageButton E4;
+    ImageButton F4;
+    ImageButton G4;
+    ImageButton H4;
+    ImageButton A5;
+    ImageButton B5;
+    ImageButton C5;
+    ImageButton D5;
+    ImageButton E5;
+    ImageButton F5;
+    ImageButton G5;
+    ImageButton H5;
+    ImageButton A6;
+    ImageButton B6;
+    ImageButton C6;
+    ImageButton D6;
+    ImageButton E6;
+    ImageButton F6;
+    ImageButton G6;
+    ImageButton H6;
+    ImageButton A7;
+    ImageButton B7;
+    ImageButton C7;
+    ImageButton D7;
+    ImageButton E7;
+    ImageButton F7;
+    ImageButton G7;
+    ImageButton H7;
+    ImageButton A8;
+    ImageButton B8;
+    ImageButton C8;
+    ImageButton D8;
+    ImageButton E8;
+    ImageButton F8;
+    ImageButton G8;
+    ImageButton H8;
     private WebSocketClient WebSocket;
 
     String URLConcatenation = "";
@@ -39,77 +103,77 @@ public class ChessActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chess);
 
-        ImageButton A1 = findViewById(R.id.A1);
-        ImageButton B1 = findViewById(R.id.B1);
-        ImageButton C1 = findViewById(R.id.C1);
-        ImageButton D1 = findViewById(R.id.D1);
-        ImageButton E1 = findViewById(R.id.E1);
-        ImageButton F1 = findViewById(R.id.F1);
-        ImageButton G1 = findViewById(R.id.G1);
-        ImageButton H1 = findViewById(R.id.H1);
+        A1 = findViewById(R.id.A1);
+        B1 = findViewById(R.id.B1);
+        C1 = findViewById(R.id.C1);
+        D1 = findViewById(R.id.D1);
+        E1 = findViewById(R.id.E1);
+        F1 = findViewById(R.id.F1);
+        G1 = findViewById(R.id.G1);
+        H1 = findViewById(R.id.H1);
 
-        ImageButton A2 = findViewById(R.id.A2);
-        ImageButton B2 = findViewById(R.id.B2);
-        ImageButton C2 = findViewById(R.id.C2);
-        ImageButton D2 = findViewById(R.id.D2);
-        ImageButton E2 = findViewById(R.id.E2);
-        ImageButton F2 = findViewById(R.id.F2);
-        ImageButton G2 = findViewById(R.id.G2);
-        ImageButton H2 = findViewById(R.id.H2);
+        A2 = findViewById(R.id.A2);
+        B2 = findViewById(R.id.B2);
+        C2 = findViewById(R.id.C2);
+        D2 = findViewById(R.id.D2);
+        E2 = findViewById(R.id.E2);
+        F2 = findViewById(R.id.F2);
+        G2 = findViewById(R.id.G2);
+        H2 = findViewById(R.id.H2);
 
-        ImageButton A3 = findViewById(R.id.A3);
-        ImageButton B3 = findViewById(R.id.B3);
-        ImageButton C3 = findViewById(R.id.C3);
-        ImageButton D3 = findViewById(R.id.D3);
-        ImageButton E3 = findViewById(R.id.E3);
-        ImageButton F3 = findViewById(R.id.F3);
-        ImageButton G3 = findViewById(R.id.G3);
-        ImageButton H3 = findViewById(R.id.H3);
+        A3 = findViewById(R.id.A3);
+        B3 = findViewById(R.id.B3);
+        C3 = findViewById(R.id.C3);
+        D3 = findViewById(R.id.D3);
+        E3 = findViewById(R.id.E3);
+        F3 = findViewById(R.id.F3);
+        G3 = findViewById(R.id.G3);
+        H3 = findViewById(R.id.H3);
 
-        ImageButton A4 = findViewById(R.id.A4);
-        ImageButton B4 = findViewById(R.id.B4);
-        ImageButton C4 = findViewById(R.id.C4);
-        ImageButton D4 = findViewById(R.id.D4);
-        ImageButton E4 = findViewById(R.id.E4);
-        ImageButton F4 = findViewById(R.id.F4);
-        ImageButton G4 = findViewById(R.id.G4);
-        ImageButton H4 = findViewById(R.id.H4);
+        A4 = findViewById(R.id.A4);
+        B4 = findViewById(R.id.B4);
+        C4 = findViewById(R.id.C4);
+        D4 = findViewById(R.id.D4);
+        E4 = findViewById(R.id.E4);
+        F4 = findViewById(R.id.F4);
+        G4 = findViewById(R.id.G4);
+        H4 = findViewById(R.id.H4);
 
-        ImageButton A5 = findViewById(R.id.A5);
-        ImageButton B5 = findViewById(R.id.B5);
-        ImageButton C5 = findViewById(R.id.C5);
-        ImageButton D5 = findViewById(R.id.D5);
-        ImageButton E5 = findViewById(R.id.E5);
-        ImageButton F5 = findViewById(R.id.F5);
-        ImageButton G5 = findViewById(R.id.G5);
-        ImageButton H5 = findViewById(R.id.H5);
+        A5 = findViewById(R.id.A5);
+        B5 = findViewById(R.id.B5);
+        C5 = findViewById(R.id.C5);
+        D5 = findViewById(R.id.D5);
+        E5 = findViewById(R.id.E5);
+        F5 = findViewById(R.id.F5);
+        G5 = findViewById(R.id.G5);
+        H5 = findViewById(R.id.H5);
 
-        ImageButton A6 = findViewById(R.id.A6);
-        ImageButton B6 = findViewById(R.id.B6);
-        ImageButton C6 = findViewById(R.id.C6);
-        ImageButton D6 = findViewById(R.id.D6);
-        ImageButton E6 = findViewById(R.id.E6);
-        ImageButton F6 = findViewById(R.id.F6);
-        ImageButton G6 = findViewById(R.id.G6);
-        ImageButton H6 = findViewById(R.id.H6);
+        A6 = findViewById(R.id.A6);
+        B6 = findViewById(R.id.B6);
+        C6 = findViewById(R.id.C6);
+        D6 = findViewById(R.id.D6);
+        E6 = findViewById(R.id.E6);
+        F6 = findViewById(R.id.F6);
+        G6 = findViewById(R.id.G6);
+        H6 = findViewById(R.id.H6);
 
-        ImageButton A7 = findViewById(R.id.A7);
-        ImageButton B7 = findViewById(R.id.B7);
-        ImageButton C7 = findViewById(R.id.C7);
-        ImageButton D7 = findViewById(R.id.D7);
-        ImageButton E7 = findViewById(R.id.E7);
-        ImageButton F7 = findViewById(R.id.F7);
-        ImageButton G7 = findViewById(R.id.G7);
-        ImageButton H7 = findViewById(R.id.H7);
+        A7 = findViewById(R.id.A7);
+        B7 = findViewById(R.id.B7);
+        C7 = findViewById(R.id.C7);
+        D7 = findViewById(R.id.D7);
+        E7 = findViewById(R.id.E7);
+        F7 = findViewById(R.id.F7);
+        G7 = findViewById(R.id.G7);
+        H7 = findViewById(R.id.H7);
 
-        ImageButton A8 = findViewById(R.id.A8);
-        ImageButton B8 = findViewById(R.id.B8);
-        ImageButton C8 = findViewById(R.id.C8);
-        ImageButton D8 = findViewById(R.id.D8);
-        ImageButton E8 = findViewById(R.id.E8);
-        ImageButton F8 = findViewById(R.id.F8);
-        ImageButton G8 = findViewById(R.id.G8);
-        ImageButton H8 = findViewById(R.id.H8);
+        A8 = findViewById(R.id.A8);
+        B8 = findViewById(R.id.B8);
+        C8 = findViewById(R.id.C8);
+        D8 = findViewById(R.id.D8);
+        E8 = findViewById(R.id.E8);
+        F8 = findViewById(R.id.F8);
+        G8 = findViewById(R.id.G8);
+        H8 = findViewById(R.id.H8);
 
         Draft[] drafts = {new Draft_6455()};
 
@@ -129,23 +193,29 @@ public class ChessActivity extends AppCompatActivity {
                     String[] strings = message.split(" ");
 
                     switch (strings[0]){
+
                         case "OpponentMoved":
-                            updateSquare();
-                            enableUserButtons();
+                            updateSquare(); //updates board accordingly
+                            enableButtons(); //enables buttons after opponent's turn
                             break;
+
                         case "UserMoved":
                             updateSquare(); //updates board accordingly
-                            enableOpponentButtons();
+                            disableButtons(); //disables buttons after user's turn
                             break;
+
                         case "Check":
-                            isCheck();
+                            isCheck(); //checks if the user is in check
                             break;
+
                         case "Checkmate":
                             isCheckmate();  //if the case is a checkmate it will run through this method and end the game
                             break;
+
                         case "UserWin":
                             System.out.println("You Won!");
                             break;
+
                         case "UserLoss":
                             System.out.println("You Lost!");
                             break;
@@ -563,12 +633,164 @@ public class ChessActivity extends AppCompatActivity {
 
     }
 
-    public void enableUserButtons(){
+    public void enableButtons(){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
 
+                A1.setClickable(true);
+                B1.setClickable(true);
+                C1.setClickable(true);
+                D1.setClickable(true);
+                E1.setClickable(true);
+                F1.setClickable(true);
+                G1.setClickable(true);
+                H1.setClickable(true);
+
+                A2.setClickable(true);
+                B2.setClickable(true);
+                C2.setClickable(true);
+                D2.setClickable(true);
+                E2.setClickable(true);
+                F2.setClickable(true);
+                G2.setClickable(true);
+                H2.setClickable(true);
+
+                A3.setClickable(true);
+                B3.setClickable(true);
+                C3.setClickable(true);
+                D3.setClickable(true);
+                E3.setClickable(true);
+                F3.setClickable(true);
+                G3.setClickable(true);
+                H3.setClickable(true);
+
+                A4.setClickable(true);
+                B4.setClickable(true);
+                C4.setClickable(true);
+                D4.setClickable(true);
+                E4.setClickable(true);
+                F4.setClickable(true);
+                G4.setClickable(true);
+                H4.setClickable(true);
+
+                A5.setClickable(true);
+                B5.setClickable(true);
+                C5.setClickable(true);
+                D5.setClickable(true);
+                E5.setClickable(true);
+                F5.setClickable(true);
+                G5.setClickable(true);
+                H5.setClickable(true);
+
+                A6.setClickable(true);
+                B6.setClickable(true);
+                C6.setClickable(true);
+                D6.setClickable(true);
+                E6.setClickable(true);
+                F6.setClickable(true);
+                G6.setClickable(true);
+                H6.setClickable(true);
+
+                A7.setClickable(true);
+                B7.setClickable(true);
+                C7.setClickable(true);
+                D7.setClickable(true);
+                E7.setClickable(true);
+                F7.setClickable(true);
+                G7.setClickable(true);
+                H7.setClickable(true);
+
+                A8.setClickable(true);
+                B8.setClickable(true);
+                C8.setClickable(true);
+                D8.setClickable(true);
+                E8.setClickable(true);
+                F8.setClickable(true);
+                G8.setClickable(true);
+                H8.setClickable(true);
+            }
+        });
     }
 
-    public void enableOpponentButtons(){
+    public void disableButtons(){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
 
+                A1.setClickable(false);
+                B1.setClickable(false);
+                C1.setClickable(false);
+                D1.setClickable(false);
+                E1.setClickable(false);
+                F1.setClickable(false);
+                G1.setClickable(false);
+                H1.setClickable(false);
+
+                A2.setClickable(false);
+                B2.setClickable(false);
+                C2.setClickable(false);
+                D2.setClickable(false);
+                E2.setClickable(false);
+                F2.setClickable(false);
+                G2.setClickable(false);
+                H2.setClickable(false);
+
+                A3.setClickable(false);
+                B3.setClickable(false);
+                C3.setClickable(false);
+                D3.setClickable(false);
+                E3.setClickable(false);
+                F3.setClickable(false);
+                G3.setClickable(false);
+                H3.setClickable(false);
+
+                A4.setClickable(false);
+                B4.setClickable(false);
+                C4.setClickable(false);
+                D4.setClickable(false);
+                E4.setClickable(false);
+                F4.setClickable(false);
+                G4.setClickable(false);
+                H4.setClickable(false);
+
+                A5.setClickable(false);
+                B5.setClickable(false);
+                C5.setClickable(false);
+                D5.setClickable(false);
+                E5.setClickable(false);
+                F5.setClickable(false);
+                G5.setClickable(false);
+                H5.setClickable(false);
+
+                A6.setClickable(false);
+                B6.setClickable(false);
+                C6.setClickable(false);
+                D6.setClickable(false);
+                E6.setClickable(false);
+                F6.setClickable(false);
+                G6.setClickable(false);
+                H6.setClickable(false);
+
+                A7.setClickable(false);
+                B7.setClickable(false);
+                C7.setClickable(false);
+                D7.setClickable(false);
+                E7.setClickable(false);
+                F7.setClickable(false);
+                G7.setClickable(false);
+                H7.setClickable(false);
+
+                A8.setClickable(false);
+                B8.setClickable(false);
+                C8.setClickable(false);
+                D8.setClickable(false);
+                E8.setClickable(false);
+                F8.setClickable(false);
+                G8.setClickable(false);
+                H8.setClickable(false);
+            }
+        });
     }
     public void isCheck(){
         
