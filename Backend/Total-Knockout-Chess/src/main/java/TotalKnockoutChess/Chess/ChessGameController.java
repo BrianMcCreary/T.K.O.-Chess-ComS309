11a8c656fc77bb6cf339efa8499807d24f6f84ce
Brian McCreary <brianmcc@iastate.edu>
2023-04-23 16:30:38 -0500

    Chess progress

--- Backend/Total-Knockout-Chess/src/main/java/TotalKnockoutChess/Chess/ChessGameController.java
+++ Backend/Total-Knockout-Chess/src/main/java/TotalKnockoutChess/Chess/ChessGameController.java
@@ -4,9 +4,12 @@ import TotalKnockoutChess.Lobby.Lobby;
 import TotalKnockoutChess.Lobby.LobbyRepository;
 import TotalKnockoutChess.Users.User;
 import TotalKnockoutChess.Users.UserRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Api(value = "ChessGameController", description = "Controller used to manage ChessGame entities")
 @RestController
 @RequestMapping("/chess")
 public class ChessGameController {
@@ -24,6 +27,7 @@ public class ChessGameController {
     private final String success = "{\"message\":\"success\"}";
     private final String failure = "{\"message\":\"failure\"}";
 
+    @ApiOperation(value = "Creates a new ChessGame instance and saves it to the repository")
     @PostMapping("/{lobbyCode}")
     public void createChessGame(@PathVariable Long lobbyCode){
         Lobby lobby = lobbyRepository.getByCode(lobbyCode);
