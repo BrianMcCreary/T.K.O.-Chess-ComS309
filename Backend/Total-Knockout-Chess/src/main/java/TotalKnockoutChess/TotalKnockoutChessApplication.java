f3ebbf134cb42088a675a46a941f9b3edd98c341
Connor <connor66@iastate.edu>
2023-05-04 15:14:42 -0500

    redeploy

--- Backend/Total-Knockout-Chess/src/main/java/TotalKnockoutChess/TotalKnockoutChessApplication.java
+++ Backend/Total-Knockout-Chess/src/main/java/TotalKnockoutChess/TotalKnockoutChessApplication.java
@@ -3,7 +3,7 @@ package TotalKnockoutChess;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Commenting here so the server re-deploys 8
+//Commenting here so the server re-deploys 9
 
 @SpringBootApplication
 public class TotalKnockoutChessApplication {
