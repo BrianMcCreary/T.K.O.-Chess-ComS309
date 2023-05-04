74de1a66a2c3459c2844cab6cdc87dd5ef7ded48
Zachary Scurlock <scurlock@iastate.edu>
2023-05-04 15:43:29 -0500

    m

--- Frontend/T.K.O_Chess/app/src/main/java/com/example/tko_chess/ultils/Const.java
+++ Frontend/T.K.O_Chess/app/src/main/java/com/example/tko_chess/ultils/Const.java
@@ -7,13 +7,21 @@ public class Const {
 
     public static final String URL_SERVER_AN5 = "http://coms-309-005.class.las.iastate.edu:8080";
 
+
+
+    public static final String URL_SERVER_GETUSERS = "http://coms-309-005.class.las.iastate.edu:8080/getusers";
+
+
+
     public static final String URL_SERVER_USERS = "http://coms-309-005.class.las.iastate.edu:8080/users/";
 
     public static final String URL_SERVER_LOGIN = "http://coms-309-005.class.las.iastate.edu:8080/users/login";
 
 
 
-    public static final String URL_SERVER_CHANGEUSERNAME = "http://coms-309-005.class.las.iastate.edu:8080/users/name/";
+    public static final String URL_SERVER_GETUSERSTATS = "http://coms-309-005.class.las.iastate.edu:8080/getUserStats/";
+
+    public static final String URL_SERVER_CHANGEUSERNAME = "http://coms-309-005.class.las.iastate.edu:8080/users/username/";
 
     public static final String URL_SERVER_CHANGEPASSWORD = "http://coms-309-005.class.las.iastate.edu:8080/users/password/";
 
