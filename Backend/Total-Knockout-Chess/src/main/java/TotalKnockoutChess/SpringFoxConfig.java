f72a50ddcd9fdd4d2b1e649941796dc4c9ce159c
Connor <connor66@iastate.edu>
2023-05-04 23:48:33 -0500

    Fixing backend API

--- Backend/Total-Knockout-Chess/src/main/java/TotalKnockoutChess/SpringFoxConfig.java
+++ Backend/Total-Knockout-Chess/src/main/java/TotalKnockoutChess/SpringFoxConfig.java
@@ -11,7 +11,6 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableWebMvc
 @EnableSwagger2
 @Import(SpringDataRestConfiguration.class)
 @Configuration
