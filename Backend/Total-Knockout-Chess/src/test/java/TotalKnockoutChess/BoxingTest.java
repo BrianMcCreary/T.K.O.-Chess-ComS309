8b71d33de2231daf8a7c40d99c11596e2394b73e
Connor <connor66@iastate.edu>
2023-05-04 22:29:00 -0500

    redeploy

--- Backend/Total-Knockout-Chess/src/test/java/TotalKnockoutChess/BoxingTest.java
+++ Backend/Total-Knockout-Chess/src/test/java/TotalKnockoutChess/BoxingTest.java
@@ -1,35 +1,35 @@
-package TotalKnockoutChess;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-
-/**
- * @author Connor Hand
- */
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@RunWith(SpringRunner.class)
-public class BoxingTest {
-    @LocalServerPort
-    int port;
-
-    @Before
-    public void setUp() {
-        RestAssured.port = port;
-        RestAssured.baseURI = "http://localhost";
-    }
+//package TotalKnockoutChess;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//import org.json.JSONArray;
+//import org.json.JSONException;
+//import org.json.JSONObject;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import io.restassured.RestAssured;
+//import io.restassured.response.Response;
+//
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.web.server.LocalServerPort;
+//
+///**
+// * @author Connor Hand
+// */
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@RunWith(SpringRunner.class)
+//public class BoxingTest {
+//    @LocalServerPort
+//    int port;
+//
+//    @Before
+//    public void setUp() {
+//        RestAssured.port = port;
+//        RestAssured.baseURI = "http://localhost";
+//    }
 
 //    @Test
 //    public void gameCreationAndDeletionTest() {
@@ -92,4 +92,4 @@ public class BoxingTest {
 //            e.printStackTrace();
 //        }
 //    }
-}
+//}
